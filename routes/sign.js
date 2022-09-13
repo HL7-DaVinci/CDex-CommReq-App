af41392d8276aabf189a88f48ec2698884f5c6d6
Karell <karellr@gmail.com>
2022-09-13 13:43:12 -0500

    Post request test to sign

--- routes/sign.js
+++ routes/sign.js
@@ -6,13 +6,13 @@ const canonicalize = require('canonicalize')
 const jose = require('node-jose')
 const fetch = require('node-fetch')
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await response.json();    
   res.json(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/try', async (req, res) => {
   const certPath = path.join(__dirname, '../config/cert.pem')
   const cert = fs.readFileSync(certPath)
 
@@ -54,6 +54,6 @@ router.post('/', async (req, res) => {
   }
 
   res.json(sigElement)
-})
+});
 
 module.exports = router
